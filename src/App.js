76ebe7b4968788e36e682d2a8e57b4799e7aed9c
Kandy-Hamisi <hkandy2000@gmail.com>
2022-05-23 20:48:02 +0300

    :heavy_minus_sign: removed the test chartjs/react-chart-js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { Line} from 'react-chartjs-2';
-
+import './App.css';
 const App = () => {
-  
-  const data = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-    datasets: [
-      {
-        label: 'Sales for 2022 (M)',
-        data: [3, 4, 5, 6, 2],
-      },
-    ],
-  };
-
   return (
-    <div>
-      <Line data={data}/>
+    <div className='App'>
+      <h1>Hello React 18</h1>
     </div>
   )
 }
